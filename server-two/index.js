2e19f045c64bc607582e55d950f7ba2b0ec95c4f
Tomi Jumppanen <tomi.jumppanen@hotmail.com>
2022-10-27 16:20:38 +0300

    Instead of serving .html directly we convert .pug to htmll

---
+++ server-two/index.js
@@ -0,0 +1,24 @@
+'use strict';
+const express=require('express');
+const pug=require('pug');
+const app=express();
+const port=3000;
+
+app.use('/', express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send(pug.renderFile('views/template.pug'))
+});
+
+app.get("/catinfo", (req, res) => {
+    const cat={
+        name: "Frank",
+        birthdate: "2010-12-25",
+        weight: 5,
+    };
+    res.json(cat);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
