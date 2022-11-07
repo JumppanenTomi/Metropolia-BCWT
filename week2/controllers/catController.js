cce146905bb4db817a12e6495a4d1214689e33c6
Tomi Jumppanen <tomi.jumppanen@hotmail.com>
2022-11-07 19:38:08 +0200

    Added new export

--- week2/controllers/catController.js
+++ week2/controllers/catController.js
@@ -9,6 +9,10 @@ const cat_list_get=(req, res) => {
     res.json(cats);
 };
 
+const cat_get=(req, res) => {
+    res.json(cats.filter(cat => cat.id==req.params.id));
+};
+
 module.exports={
-    cat_list_get,
+    cat_list_get, cat_get
 };
