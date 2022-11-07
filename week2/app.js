a4ba283f36a9d6b5d29b8016f17bb3f235774a61
Tomi Jumppanen <tomi.jumppanen@hotmail.com>
2022-11-07 19:37:51 +0200

    Added user router

--- week2/app.js
+++ week2/app.js
@@ -1,8 +1,10 @@
 'use strict';
 const express=require('express');
-const catRouter=require('./routes/catRouter.js')
+const catRoutes=require('./routes/catRoutes.js')
+const userRoutes=require('./routes/userRoutes.js')
 const app=express();
 const port=3000;
 
-app.use('/cat', catRouter)
+app.use('/cat', catRoutes)
+app.use('/user', userRoutes)
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
