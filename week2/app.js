2f5311d681c594e441f81c460c9df27a2d1e1665
Tomi Jumppanen <tomi.jumppanen@hotmail.com>
2022-11-07 18:19:16 +0200

    Added post, put, delete routes

---
+++ week2/app.js
@@ -0,0 +1,22 @@
+'use strict';
+const express=require('express');
+const app=express();
+const port=3000;
+
+app.get('/cat', (req, res) => {
+  res.send('From this endpoint you can get cats.')
+});
+
+app.post('/cat/add', (req, res) => {
+  res.send('Here u add cats')
+})
+
+app.put('/cat/edit', (req, res) => {
+  res.send('Here u modify cats')
+})
+
+app.delete('/cat/delete', (req, res) => {
+  res.send('Here u terminate cats')
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
