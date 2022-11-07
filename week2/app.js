3951e728b5793c284e4b7702e58658f2b1e958e8
Tomi Jumppanen <tomi.jumppanen@hotmail.com>
2022-11-07 18:41:51 +0200

    Adde router

--- week2/app.js
+++ week2/app.js
@@ -1,26 +1,8 @@
 'use strict';
 const express=require('express');
+const catRouter=require('./routes/catRouter.js')
 const app=express();
 const port=3000;
 
-app.get('/cat', (req, res) => {
-  res.send('From this endpoint you can get cats.')
-});
-
-app.get('/cat/:id', (req, res) => {
-  res.send('From this endpoint you can get cats. '+req.params.id)
-});
-
-app.post('/cat/add', (req, res) => {
-  res.send('Here u add cats')
-})
-
-app.put('/cat/edit', (req, res) => {
-  res.send('Here u modify cats')
-})
-
-app.delete('/cat/delete', (req, res) => {
-  res.send('Here u terminate cats')
-})
-
+app.use('/cat', catRouter)
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
