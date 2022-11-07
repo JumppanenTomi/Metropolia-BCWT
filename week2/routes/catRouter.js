4eb9cf99a9fbae424f877aaabcc414f048f092b6
Tomi Jumppanen <tomi.jumppanen@hotmail.com>
2022-11-07 18:42:35 +0200

    Added router

---
+++ week2/routes/catRouter.js
@@ -0,0 +1,9 @@
+// Router
+"use strict";
+const express=require("express");
+const router=express.Router();
+const catController=require("../controllers/catController");
+
+router.get("/", catController.cat_list_get);
+
+module.exports=router;
