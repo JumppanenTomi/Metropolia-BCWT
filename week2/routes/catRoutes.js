c498d5775a243b5f9399d06155a13acdf82c5bbc
Tomi Jumppanen <tomi.jumppanen@hotmail.com>
2022-11-08 11:37:57 +0200

    Tried to work with multer

--- week2/routes/catRoutes.js
+++ week2/routes/catRoutes.js
@@ -3,9 +3,16 @@
 const express=require("express");
 const router=express.Router();
 const catController=require("../controllers/catController");
+const multer=require('multer')
+const upload=multer({ dest: 'uploads/' })
 
 router.get("/", catController.cat_list_get);
 
 router.get("/:id", catController.cat_get);
+
+router.post('/', upload.array('file', 12), function (req, res, next) {
+    // req.files is array of `photos` files
+    // req.body will contain the text fields, if there were any
+})
 
 module.exports=router;
