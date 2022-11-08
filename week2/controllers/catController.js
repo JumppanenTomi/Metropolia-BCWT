f711016201d9757b4b234db7cc0fbfbb5523d46a
Tomi Jumppanen <tomi.jumppanen@hotmail.com>
2022-11-08 11:36:44 +0200

    Added createCat

--- week2/controllers/catController.js
+++ week2/controllers/catController.js
@@ -13,6 +13,10 @@ const cat_get=(req, res) => {
     res.json(cats.filter(cat => cat.id==req.params.id));
 };
 
+const createCat=(res, req) => {
+    res.json("file received")
+};
+
 module.exports={
-    cat_list_get, cat_get
+    cat_list_get, cat_get, createCat
 };
